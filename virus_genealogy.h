421f98d38c66d5db7e323c7f70587db035820512
Michał Woś <wosiu@students.mimuw.edu.pl>
2013-12-21 23:25:36 +0100

    kompiluje sie, assert w kodzie sie rzuca

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -11,6 +11,8 @@
 #include <map>
 #include <algorithm>
 #include <iostream>
+#include <memory>
+#include <cassert>
 
 /* Drogi Łukaszu! Informacje o stylu, jakiego używałem poniżej:
 1. po nawiasie i przed nawiasem zawsze spacja, no chyba że puste ()
@@ -63,10 +65,12 @@ public:
 	typedef typename Virus::id_type id_type;
 	id_type rootId;
 	struct Node;
+	shared_ptr <Node>rootNode;
 	// komparator do grafu (ktory jest set'em), porownuje wierzcholki wzgledem
 	// id wirusa
 	#define node second
-	typedef std::map <id_type, Node> graph_type;
+	typedef Node* parent_type;
+	typedef std::map <id_type, parent_type> graph_type;
 	graph_type graph;
 	typedef typename graph_type::iterator graph_it;
 
@@ -74,22 +78,33 @@ public:
 	struct Node
 	{
 		Virus virus;
-		// komparator do porównywania iteratorów w setach ojców / dzieci
-		struct classcomp
-		{
-			bool operator() ( const graph_it node1, const graph_it node2 ) const
-			{
-				return node1->node.get_id() < node2->node.get_id();
-			}
-		};
-		// w secie trzymamy iteratory do grafu (czyli set'a wszystkich wieszchołków)
-		// dzieki temu oszczedzamy log(n) na wyszkuiwaniu pojedynczego ojca / syna
-		// np podczas usuwania
-		std::set <graph_it, classcomp> _parents;
-		std::set <graph_it, classcomp> _children;
+		std::set <parent_type > _parents;
+		std::set <shared_ptr<Node> > _children;
+		graph_it me_in_graph;
+		graph_type* _my_graph = nullptr;
 
-		Node( id_type const &id ) noexcept : virus( Virus(id) )
+		Node( id_type const &id, graph_type &my_graph ) noexcept : virus( Virus(id) ), _my_graph( &my_graph )
 		{
+		}
+
+		~Node()
+		{
+			// skoro uruchomil sie destruktor, to nic nie wskazuje na ten
+			// wierzcholek nie powinien miec zadnych rodzicow
+			assert( _parents.empty() );
+			while ( !_children.empty() ) {
+				auto sit = _children.begin();
+				auto child = *sit; //shared_ptr
+				// usuwam obecny wierzcholek w tablicach rodzicow jego dzieci
+				child->_parents.erase( this );
+				// usuwam dziecko tego wierzcholka
+				// tu wywola sie destruktor owego dziecka, jesli obecny
+				// wierzcholek byl jego ostatnim ojcem
+				_children.erase( sit );
+			}
+			// usuwam obecne mapowanie id -> wskaznik na obecny wierzcholek
+			// z grafu
+			_my_graph->erase( me_in_graph );
 		}
 
 		/*
@@ -103,21 +118,22 @@ public:
 			return virus.get_id();
 		}
 
-		void add_parent( graph_it parent )
+		void add_parent( parent_type parent )
 		{
 			_parents.insert( parent );
 		}
 
-		void add_children( graph_it children )
+		void add_children( Node* children )
 		{
-			_children.insert( children );
+			_children.insert( shared_ptr<Node>( children ) );
 		}
 
 		vector<id_type> get_children_ids() const
 		{
 			std::vector<id_type> res;
 			for ( auto child : _children ) {
-				res.push_back( child->node.get_id() );
+				assert ( child.use_count() > 0 );
+				res.push_back( child->get_id() );
 			}
 			return res;
 		}
@@ -125,45 +141,21 @@ public:
 		vector<id_type> get_parents_ids() const
 		{
 			std::vector<id_type> res;
-			for ( graph_it parent : _parents ) {
-				res.push_back( parent->node.get_id() );
+			for ( auto parent : _parents ) {
+				assert ( parent != NULL );
+				res.push_back( parent->get_id() );
 			}
 			return res;
 		}
 	};
-
-/*	void bug()
-	{
-		graph.insert( make_pair( "bla", Node( "bla" ) ) );
-		cout << graph.size() << endl;
-		graph_it node1,node2;
-		node1 = graph.begin();
-		node2 = graph.begin();
-		node2++;
-		cout << node1->second.get_id() << endl;
-		cout << node2->second.get_id() << endl;
-		cout << node1->second.tmp.size() << endl;
-
-		//vector<graph_it>bla;
-		//bla.push_back(node2);
-		// i niby to samo robie ponizej, tylko wewnatrz node1, a sie wyklada:
-		node1->second.tmp.push_back( node2 );
-		node1->second.tmp_int = 42;
-		//graph_it = graph.begin();
-		//newNode->_parents.insert( graph.begin() );
-
-		Node lokalny_node("asd");
-		lokalny_node.tmp_int = 42;
-	}
-*/
-
 
 //public:
 	// Tworzy nową genealogię.
 	// Tworzy także węzęł wirusa macierzystego o identyfikatorze stem_id.
 	VirusGenealogy( id_type const &stem_id ) noexcept
 	{
-		graph.insert( make_pair( stem_id, Node( stem_id ) ) );
+		Node* nd = new Node( stem_id, graph );
+		graph.insert( make_pair( stem_id, nd ) );
 		rootId = stem_id;
 	}
 
@@ -182,7 +174,7 @@ public:
 		if ( it == graph.end() ) {
 			throw VirusNotFound();
 		}
-		return it->node.get_children_ids();
+		return it->node->get_children_ids();
 	}
 
 	// Zwraca listę identyfikatorów bezpośrednich poprzedników wirusa
@@ -194,7 +186,7 @@ public:
 		if ( it == graph.end() ) {
 			throw VirusNotFound();
 		}
-		return it->node.get_parents_ids();
+		return it->node->get_parents_ids();
 	}
 
 	// Sprawdza, czy wirus o podanym identyfikatorze istnieje.
@@ -210,13 +202,13 @@ public:
 	// tyle ze z referencja daje blad kompilacji
 	// "If the map object is const-qualified, the function returns a const_iterator. Otherwise, it returns an iterator."
 	// no i u nas jest wlasnie to nieszczesne "const-qualified"
-	Virus operator[]( id_type const &id ) const
+	Virus& operator[]( id_type const &id ) const
 	{
 		auto it = graph.find( id );
 		if ( it == graph.end() ) {
 			throw VirusNotFound();
 		}
-		return it->node.virus;
+		return it->node->virus;
 	}
 	// Tworzy węzęł reprezentujący nowy wirus o identyfikatorze id
 	// powstały z wirusów o podanym identyfikatorze parent_id lub
@@ -227,32 +219,37 @@ public:
 	// poprzedników nie istnieje.
 	void create( id_type const &id, std::vector<id_type> const &parent_ids )
 	{
-		if ( parent_ids.empty() ) {
-			throw VirusNotFound();
-		}
-		// sprawdzamy, czy wierzcholek nie istnial wczesniej
-		if ( graph.find( id ) != graph.end() ) {
-			throw VirusAlreadyCreated();
-		}
-		// sprawdzamy czy wszyscy ojcowie istnieja
-		size_t n = parent_ids.size();
-		graph_it parents_it[n];
-		for ( size_t i = 0; i < n; i++ ) {
-			parents_it[i] = graph.find( parent_ids[i] );
-			if ( parents_it[i] == graph.end() ) {
+			if ( parent_ids.empty() ) {
 				throw VirusNotFound();
 			}
-		}
-		// stworzenie nowego wezla o id w grafie
-		// first daje nam iterator na umieszczony w grafie element
-		graph_it newNode = graph.insert( make_pair( id, Node( id ) ) ).first;
+			// sprawdzamy, czy wierzcholek nie istnial wczesniej
+			if ( graph.find( id ) != graph.end() ) {
+				throw VirusAlreadyCreated();
+			}
+			// sprawdzamy czy wszyscy ojcowie istnieja
+			size_t n = parent_ids.size();
+			parent_type parents[n];
+			for ( size_t i = 0; i < n; i++ ) {
+				auto parent_it = graph.find( parent_ids[i] );
+				if ( parent_it == graph.end() ) {
+					throw VirusNotFound();
+				}
+				parents[i] = parent_it->node;
+			}
+			// stworzenie nowego wezla o id w grafie
+			// fime_in_graphrst daje nam iterator na umieszczony w grafie element
+			Node* newNode = new Node( id, graph );
+			auto me_in_graph = graph.insert( make_pair( id, newNode ) ).first;
+			// zapamietuje iterator na siebie w grafie - przyda sie przy usuwaniu
+			// aby uniknac potencjalnego wyjatku przy wyszukiwaniu po id
+			newNode->me_in_graph = me_in_graph;
 
-		for ( size_t i = 0; i < n; i++ ) {
-			// przypisanie mu podanych ojcow
-			newNode->node.add_parent( parents_it[i] );
-			// a ojcom dodanie dziecka - obecnego, nowego wezla
-			parents_it[i]->node.add_children( newNode );
-		}
+			for ( size_t i = 0; i < n; i++ ) {
+				// przypisanie mu podanych ojcow
+				newNode->add_parent( parents[i] );
+				// a ojcom dodanie dziecka - obecnego, nowego wezla
+				parents[i]->add_children( newNode );
+			}
 	}
 
 	void create( id_type const &id, id_type const &parent_id )
@@ -272,8 +269,8 @@ public:
 		if ( parent == graph.end() ) {
 			throw VirusNotFound();
 		}
-		child->node.add_parent( parent );
-		parent->node.add_children( child );
+		child->node->add_parent( parent->node );
+		parent->node->add_children( child->node );
 	}
 
 	// Usuwa wirus o podanym identyfikatorze.
@@ -289,21 +286,28 @@ public:
 		if ( it == graph.end() ) {
 			throw VirusNotFound();
 		}
-		remove( it );
+
+		assert( !it->node->_parents.empty() );
+		// usuwamy wierzcholek z listy dzieci jego ojcow (odcinamy od ojcow)
+		while ( !it->node->_parents.empty() ) {
+			// bierzemy pierwszy iterator z set'a ojcow
+			auto parent_it = it->node->_parents.begin();
+			parent_type parent = *parent_it;
+			parent->_children.erase( shared_ptr<Node>(it->node) );
+			it->node->_parents.erase( parent_it );
+		}
+		// w tym momencie - dzieki temu, ze juz zaden shared_ptr nie wskazuje na
+		// ten wezel - wywola sie destruktor. Wszyscy ojcowie
+		// zostali odcieci. W destruktorze zostaną odciete dzieci (i tam byc moze
+		// beda rekurencyjnie usuwac sie wezly dalej.
 	}
 
-private:
 	void remove( graph_it it )
 	{
-		// usuwamy wierzcholek z listy dzieci jego ojcow
-		while ( !it->node._parents.empty() ) {
-			// bierzemy pierwszy iterator z set'a ojcow
-			graph_it parent = *( it->node._parents.begin() );
-			parent->node._children.erase( it );
-			it->node._parents.erase( parent );
-		}
+		// ...
+
 		// usuwanie wierzcholek z listy ojcow jego dzieci
-		while ( !it->node._children.empty() ) {
+		/*while ( !it->node._children.empty() ) {
 			graph_it child = *( it->node._children.begin() );
 			child->node._parents.erase( it );
 			it->node._children.erase( child );
@@ -312,9 +316,11 @@ private:
 			if ( child->node._parents.empty() ) {
 				remove( child );
 			}
-		}
+		}*/
 		// ojcowie oraz synowie zaktualizowani, wiec mozemy usunac z grafu
-		graph.erase( it );
+
+		//to takze stanie sie destruktorze:
+		//graph.erase( it );
 	}
 /**/
 };
